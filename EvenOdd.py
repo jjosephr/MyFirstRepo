5b435ed70d6a50d770dffa7eb060fcd66585e6c3
jjosephr <josphselvin@yahoo.com>
2018-10-11 15:05:29 +0530

    user input removed..

--- EvenOdd.py
+++ EvenOdd.py
@@ -1,6 +1,7 @@
-v = int(input("Enter a value to check its is even or odd number... "))
-if (v%2 == 0):
-    print("The enter number is Even number")
-else:
-    print("The enter number is ODD number")
+v = [35,66,77,23,2,1] #int(input("Enter a value to check its is even or odd number... "))
+for element in v:
+    if (element%2 == 0):
+        print(element, "is an Even number")
+    else:
+        print(element, "is an ODD number")
 

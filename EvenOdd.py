d406104eab93698628edcf3e24e08693a5382db1
jjosephr <josphselvin@yahoo.com>
2018-10-11 13:25:48 +0530

    Python program to find the given number is even or odd

---
+++ EvenOdd.py
@@ -0,0 +1,6 @@
+v = int(input("Enter a value to check its is even or odd number... "))
+if (v%2 == 0):
+    print("The enter number is Even number")
+else:
+    print("The enter number is ODD number")
+
